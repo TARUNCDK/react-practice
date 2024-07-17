5da445fcf177a7bd12a1dd20060055f18202acdb
Tarun C D K <taruncdk@gmail.com>
2024-07-17 23:29:43 +0530

    added hover

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
     passwordGenerator()
   } , [length , numberAllowed , charAllowed , passwordGenerator])
 
-  
+
   return (
     <>
       <div
@@ -60,7 +60,7 @@ function App() {
             ref={passwordRef}
           />
           <button onClick={copyPassword}
-          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-600 transition-colors">copy</button>
         </div>
 
         <div className="flex text-sm gap-x-2">
